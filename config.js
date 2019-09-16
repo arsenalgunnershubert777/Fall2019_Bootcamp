42fa28b7252c0f6d5aea1327d6f1635544ef71be
Hubert Zhao <hubert777@ufl.edu>
2019-09-15 21:52:23 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    db: {
-        uri: 'mongodb+srv://hube:iHrvM0OYgmucTyh@cluster0-os6pz.mongodb.net/CEN3031?retryWrites=true&w=majority', //place the URI of your mongo database here.
-    }
-};
